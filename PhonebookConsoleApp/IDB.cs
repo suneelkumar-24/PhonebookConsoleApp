5c7af62db7d38d0ca5726b5c7955663d32c5cdc9
suneel <sunee@DESKTOP-KNHHK0Q(sksolanki531@gmail.com)>
2020-09-18 17:32:56 +0500

    f

---
+++ PhonebookConsoleApp/IDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PhonebookConsoleApp
+{
+    interface IDB
+    {
+
+        DataTable executeQuery(string sqlQuery);
+
+        DataTable executeStoreProcedure(string procedureName);
+
+        DataTable executeStoreProcedure(string procedureName, List<SqlParameter> parameters);
+    }
+}
---
+++ PhonebookConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PhonebookConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBManager manager = new DBManager("dbConnString");
+            String sqlQuery = "select * from user_data";
+           //  DataTable dt = manager.executeStoreProcedure("getAllUser");
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@userID", 2));
+
+            DataTable dt = manager.executeStoreProcedure("getUser", parameters);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine(row["user_id"].ToString() + "----" + row["user_name"].ToString());
+            }
+
+        }
+    }
+}
